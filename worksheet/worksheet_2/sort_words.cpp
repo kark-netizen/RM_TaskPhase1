34e0829defce2cf63ee1ae30bd7422c5942c00a0
Kartikeya Tiwari <75495474+kark-netizen@users.noreply.github.com>
2020-12-13 23:01:06 +0530

    adding worksheet 2 with fixed errors

--- worksheet/worksheet_2/sort_words.cpp
+++ worksheet/worksheet_2/sort_words.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int stringLength(char *ptr) 
 { 
@@ -14,42 +15,43 @@ void copyString(char *ptr, int length, char copy[])
     int i=0;
     while(*ptr!='\0')
     {
-
         ptr++;
         cout<<*ptr<<endl;
     }
 }
-void Bubble_Sort(int a[], int size, int inputString[][6])
+void Selection_Sort(int *length, int n, string words[] )
 {
-	for(int i=0;i<size;i++)
+	int ptr;
+	for(int i=0;i<n;i++)
 	{
-		for(int j=0;j<size-1;j++)
+		ptr=i;
+		for(int j=i+1;j<n-1;j++)
 		{
-			if(a[j]>a[j+1])
-			{
-				int t=a[j];
-				a[j]=a[j+1];
-				a[j+1]=t;
-                char te[4]=inputString[j];
-                inputString[j]=inputString[j+1];
-                inputString[j+1]=te;
-			}
+			if(length[ptr]>length[j])
+				ptr=j;
 		}
-	}
+		int t=length[ptr];
+		length[ptr]=length[i];
+		length[i]=t;
+        string word=words[ptr];
+        words[ptr]=words[i];
+        words[i]=word;
+    }
+    return;
 }
 int main()
 {
     int r=4,c=5;
-    char inputString[r][c] = {  "hey", "i" , "am" , "C3PO" };
+    string inputString[r]= {  "hey", "i" , "am" , "C3PO" };
     int len[r];
+    for(int i=0;i<r;i++)//calculates length of each word
+    {
+        len[i]=inputString[i].length();
+    }
+    Selection_Sort(len,r,inputString);
     for(int i=0;i<r;i++)
     {
-        len[i]=stringLength(inputString[i]);
-    }
-    char a[]=inputString[1];
-
-    Bubble_Sort(len,r,inputString);
-
-
+        cout<<inputString[i]<<endl;
+    }   
 
 }
